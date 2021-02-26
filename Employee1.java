2d33b9c7a9a9b1803326b85a7e7802491f39d67a
harshit3610 <46259317+harshit3610@users.noreply.github.com>
2021-02-26 17:10:23 +0530

    Final Task

---
+++ Employee1.java
@@ -0,0 +1,29 @@
+public class employee 
+{
+                            int id;
+    double salary=0.0;
+
+    public employee(int Id) {
+        this.Id = id;
+    }
+
+    public employee() {
+                            }
+
+    double calTax(){
+           if(this.Salary > 500000.00 && this.salary < 1000000)
+                 return this.salary*0.1;
+
+                         elseif(this.salary >                         1000000.00 && this.Salary < 1500000)
+                                    return this.Salary*0.2;
+
+        else
+                            return this.Salary*0.3;
+        
+        for(int i=0;i<10;i++){
+            
+        }
+
+    }
+
+}
