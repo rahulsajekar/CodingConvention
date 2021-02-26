67756fe7a9dd9d27af6d6b62a970b7470d6dcdca
rahulsajekar <rahulsajekar93@gmail.com>
2021-02-26 17:24:23 +0530

    File Corrected

---
+++ EmployeeOne.java
@@ -0,0 +1,24 @@
+public class EmployeeOne
+{
+    int id;
+    double salary=0.0;
+
+    public EmployeeOne(int Id) {
+        this.id = id;
+    }
+
+    public EmployeeOne() {
+    }
+
+    double calTax(){
+        if(this.salary > 500000.00 && this.salary < 1000000)
+            return this.salary*0.1;
+
+        else if(this.salary >1000000.00 && this.salary < 1500000)
+            return this.salary*0.2;
+
+        else return this.salary*0.3;
+
+    }
+
+}
